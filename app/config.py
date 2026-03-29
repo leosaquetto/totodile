a23e254f416f9e785c75c51f9d2e3598d072f919
leosaquetto <53850390+leosaquetto@users.noreply.github.com>
2026-03-29 15:24:00 -0300

    feat: add config and constants

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("TOKEN_TOTODILE")
+GROUP_ID = os.getenv("ID_CENTRAL_TOTODILE")
+
+GITHUB_REPO = "leosaquetto/totodile"
+BRANCH = "main"
---
+++ app/constants.py
@@ -0,0 +1,9 @@
+THREADS = {
+    "general": 1,
+    "agenda": 3,
+    "aniversarios": 4,
+    "academia": 5,
+    "remedios": 7,
+    "tarefas": 8,
+    "musica": 9
+}
